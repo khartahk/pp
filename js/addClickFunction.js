2ba7d6f45a376475c8434efe84be5b0deca0bfd9
Uros Simcic <uros@qloud.io>
2015-01-13 08:25:00 +0100

    Modal edit

--- js/addClickFunction.js
+++ js/addClickFunction.js
@@ -1,4 +1,12 @@
 function addClickFunction(){
+
+	var selectData = "";
+	for(var i in DataJSON){
+		selectData = selectData+'<option value="'+i+'">'+DataJSON[i].name+'</option>';
+	}
+
+	$( ".selectNames" ).html(selectData);
+
 	$('#myModal').modal('toggle');
 	$('#myModal').on('shown.bs.modal', function () {
 	$('#myInput').focus();

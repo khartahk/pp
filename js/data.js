ebca2ebee2d9c4107a21913a75edf89f4a68bb5f
Primoz Cankar <primoz.cankar@kupi.info>
2015-01-13 07:58:39 +0100

    moved default gantchart data to file

---
+++ js/data.js
@@ -0,0 +1,75 @@
+DataJSON = [
+  { "name": "Primož",
+    "desc": "Inception","values": 
+    [
+      {
+        "from": "Tue, 13 Jan 2015 06:30:00", 
+        "to": "Wed, 14 Jan 2015 23:30:00", 
+        "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
+        "label": "  Planni...", 
+        "customClass": "ganttRed"
+
+      }
+    ]
+  },
+  { "name": "Uroš",
+    "desc": "Inception","values": 
+    [
+      {
+        "from": "Tue, 13 Jan 2015 06:30:00", 
+        "to": "Wed, 14 Jan 2015 23:30:00", 
+        "desc": "Id: 2<br/>Name:   Gathering requirements <br/>Date: 2011-11-16 to 2011-12-03", 
+        "label": "  Gather...", 
+        "customClass": "ganttGreen"
+      }
+    ]
+  },
+  { "name": "Ana",
+    "desc": "Inception","values": 
+    [
+      {
+        "from": "Tue, 15 Jan 2015 06:30:00", 
+        "to": "Wed, 16 Jan 2015 23:30:00", 
+        "desc": "Id: 3<br/>Name:   Determine scope <br/>Date: 2011-11-30 to 2011-12-08", 
+        "label": "  Determ...", 
+        "customClass": "ganttOrange"
+      }
+    ]
+  },
+  { "name": "Živa",
+    "desc": "Iteration 1","values": 
+    [
+      {
+        "from": "Tue, 17 Jan 2015 06:30:00", 
+        "to": "Wed, 18 Jan 2015 23:30:00", 
+        "desc": "Id: 4<br/>Name:   Analysis and design <br/>Date: 2011-12-08 to 2011-12-24", 
+        "label": "  Analys..."
+      }
+    ]
+  },
+  { "name": "Jože",
+    "desc": "Iteration 1","values": 
+    [
+      {
+        "from": "Tue, 18 Jan 2015 06:30:00", 
+        "to": "Wed, 19 Jan 2015 23:30:00", 
+        "desc": "Id: 5<br/>Name:   Implementation <br/>Date: 2011-12-26 to 2012-01-17", 
+        "label": "  Implem...", 
+        "customClass": "ganttRed"
+      }
+    ]
+  },
+  { "name": "Lučka",
+    "desc": "Iteration 1","values": 
+    [
+      {
+        "from": "Tue, 17 Jan 2015 06:30:00", 
+        "to": "Wed, 20 Jan 2015 23:30:00", 
+        "desc": "Id: 6<br/>Name:   Deliver prototype <br/>Date: 2012-01-17 to 2012-01-18", 
+        "label": "  Delive...", 
+        "customClass": "ganttGreen"
+      }
+    ]
+  },
+]
+     

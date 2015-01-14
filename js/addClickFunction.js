1fc91ae3930f406f5f6a18ab61c38a02a87b2952
Primoz Cankar <primoz.cankar@qloud.io>
2015-01-14 20:07:17 +0100

    fixed multiple save button bug

--- js/addClickFunction.js
+++ js/addClickFunction.js
@@ -1,27 +1,31 @@
+function saveModal (dt,rowId){
+  console.log(DataJSON);
+  DataJSON[rowId].values.push(
+    {
+      "from": $('#datepickerFrom').val(), 
+      "to": $('#datepickerTo').val(), 
+      "desc": "Id: " + (rowId+1) + "<br/>Name: " + $('#datepickerFrom').val() + " " + $('#datepickerTo').val(), 
+      "label": $("#taskName").val()
+    }
+  );
+  gantReload();
+  $modal.modal('hide');
+}
+
 function addClickFunction(dt,rowId){
 	var selectData = "";
 	for(var i in DataJSON){
 		selectData = selectData+'<option value="'+i+'">'+DataJSON[i].name+'</option>';
 	}
-
 	$( ".selectNames" ).html(selectData);
 
   $modal = $('#myModal');
-	$modal.modal('toggle');
+	$modal.modal('show');
 	$modal.on('shown.bs.modal', function () {
     $modal.focus();
-    $(this).find("#btn-save").click(function(){
-      console.log($('#datepickerFrom').val())
-      DataJSON[rowId].values.push(
-        {
-          "from": $('#datepickerFrom').val(), 
-          "to": $('#datepickerTo').val(), 
-          "desc": "Id: " + rowId + "<br/>Name: " + $('#datepickerFrom').val() + " " + $('#datepickerTo').val(), 
-          "label": $("#taskName").val()
-        }
-      );
-      gantReload();
-      $modal.modal('hide');
-    });
+    $("#btn-save").unbind('click').one('click',function(){
+      saveModal(dt,rowId);
+    })
   });
 }
+

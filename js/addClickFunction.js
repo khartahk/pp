f4af390aca6ac205ee1fbcc79547fb89e5835504
Primoz Cankar <primoz.cankar@qloud.io>
2015-01-14 21:10:51 +0100

    fixes

--- js/addClickFunction.js
+++ js/addClickFunction.js
@@ -1,13 +1,15 @@
 function saveModal (dt,rowId){
-  console.log(DataJSON);
-  DataJSON[rowId].values.push(
-    {
-      "from": $('#datepickerFrom').val(), 
-      "to": $('#datepickerTo').val(), 
-      "desc": "Id: " + (rowId+1) + "<br/>Name: " + $('#datepickerFrom').val() + " " + $('#datepickerTo').val(), 
-      "label": $("#taskName").val()
-    }
-  );
+  var value = {
+    "from": $('#datepickerFrom').val(), 
+    "to": $('#datepickerTo').val(), 
+    "desc": "Id: " + (rowId+1) + "<br/>Name: " + $('#datepickerFrom').val() + " " + $('#datepickerTo').val(), 
+    "label": $("#taskName").val(),
+    "dt":dt,
+    "rowId":rowId,
+    "dataObj":{}
+  }
+  value.dataObj = value;
+  DataJSON[rowId].values.push(value);
   gantReload();
   $modal.modal('hide');
 }
@@ -18,7 +20,7 @@ function addClickFunction(dt,rowId){
 		selectData = selectData+'<option value="'+i+'">'+DataJSON[i].name+'</option>';
 	}
 	$( ".selectNames" ).html(selectData);
-
+  // $('#datepickerFrom').datepicker("setDate", Date(dt/1000) );
   $modal = $('#myModal');
 	$modal.modal('show');
 	$modal.on('shown.bs.modal', function () {
@@ -29,3 +31,43 @@ function addClickFunction(dt,rowId){
   });
 }
 
+function editModal (data){
+  $('#datepickerFrom').val(data.from), 
+  $('#datepickerTo').val(data.to), 
+  $('#taskDescription').val(data.desc), 
+  $("#taskName").val(data.label)
+}
+
+function editSaveModal (dt,rowId){
+  var value = {
+    "from": $('#datepickerFrom').val(), 
+    "to": $('#datepickerTo').val(), 
+    "desc": "Id: " + (rowId+1) + "<br/>Name: " + $('#datepickerFrom').val() + " " + $('#datepickerTo').val(), 
+    "label": $("#taskName").val(),
+    "dt":dt,
+    "rowId":rowId,
+    "dataObj":{}
+  }
+  value.dataObj = value;
+  DataJSON[rowId].values.push(value);
+  gantReload();
+  $modal.modal('hide');
+}
+
+function editClickFunction(data){
+  var selectData = "";
+  for(var i in DataJSON){
+    selectData = selectData+'<option value="'+i+'">'+DataJSON[i].name+'</option>';
+  }
+  $( ".selectNames" ).html(selectData);
+  $modal = $('#myModal');
+  $modal.modal('show');
+  $modal.on('shown.bs.modal', function () {
+    $modal.focus();
+    editModal(data);
+    console.log(data)
+    $("#btn-save").unbind('click').one('click',function(){
+      editSaveModal(data.dt,data.rowId);
+    })
+  });
+}
--- js/data.js
+++ js/data.js
@@ -8,8 +8,16 @@ DataJSON = [
         "to": "Wed, 14 Jan 2015 23:30:00", 
         "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
         "label": "  Planni...", 
-        "customClass": "ganttRed"
-
+        "customClass": "ganttRed",
+        "dataObj": {
+          "from": "01/13/2015", 
+          "to": "01/14/2015", 
+          "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
+          "label": "  Planni...", 
+          "dt": "Tue, 13 Jan 2015 06:30:00",
+          "rowId":0,
+          "customClass": "ganttRed"
+        }
       }
     ]
   },
@@ -22,7 +30,16 @@ DataJSON = [
         "to": "Wed, 14 Jan 2015 23:30:00", 
         "desc": "Id: 2<br/>Name:   Gathering requirements <br/>Date: 2011-11-16 to 2011-12-03", 
         "label": "  Gather...", 
-        "customClass": "ganttGreen"
+        "customClass": "ganttGreen",
+        "dataObj": {
+          "from": "01/13/2015", 
+          "to": "01/14/2015", 
+          "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
+          "label": "  Gather...", 
+          "dt": "Tue, 13 Jan 2015 06:30:00",
+          "rowId":1,
+          "customClass": "ganttGreen"
+        }
       }
     ]
   },
@@ -35,7 +52,16 @@ DataJSON = [
         "to": "Wed, 16 Jan 2015 23:30:00", 
         "desc": "Id: 3<br/>Name:   Determine scope <br/>Date: 2011-11-30 to 2011-12-08", 
         "label": "  Determ...", 
-        "customClass": "ganttOrange"
+        "customClass": "ganttOrange",
+        "dataObj": {
+          "from": "01/15/2015", 
+          "to": "01/16/2015", 
+          "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
+          "label": "  Determ...", 
+          "dt": "Tue, 15 Jan 2015 06:30:00",
+          "rowId":2,
+          "customClass": "ganttOrange"
+        }
       }
     ]
   },
@@ -47,7 +73,15 @@ DataJSON = [
         "from": "Tue, 17 Jan 2015 06:30:00", 
         "to": "Wed, 18 Jan 2015 23:30:00", 
         "desc": "Id: 4<br/>Name:   Analysis and design <br/>Date: 2011-12-08 to 2011-12-24", 
-        "label": "  Analys..."
+        "label": "  Analys...",
+        "dataObj": {
+          "from": "01/17/2015", 
+          "to": "01/18/2015", 
+          "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
+          "label": "  Analys...", 
+          "dt": "Tue, 17 Jan 2015 06:30:00",
+          "rowId":3
+        }
       }
     ]
   },
@@ -60,7 +94,16 @@ DataJSON = [
         "to": "Wed, 19 Jan 2015 23:30:00", 
         "desc": "Id: 5<br/>Name:   Implementation <br/>Date: 2011-12-26 to 2012-01-17", 
         "label": "  Implem...", 
-        "customClass": "ganttRed"
+        "customClass": "ganttRed",
+        "dataObj": {
+          "from": "01/18/2015", 
+          "to": "01/19/2015", 
+          "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
+          "label": "  Implem...", 
+          "dt": "Tue, 18 Jan 2015 06:30:00",
+          "rowId":4,
+          "customClass": "ganttRed"
+        }
       }
     ],
   },
@@ -73,7 +116,16 @@ DataJSON = [
         "to": "Wed, 20 Jan 2015 23:30:00", 
         "desc": "Id: 6<br/>Name:   Deliver prototype <br/>Date: 2012-01-17 to 2012-01-18", 
         "label": "  Delive...", 
-        "customClass": "ganttGreen"
+        "customClass": "ganttGreen",
+        "dataObj": {
+          "from": "01/17/2015", 
+          "to": "01/20/2015", 
+          "desc": "Id: 1<br/>Name:   Planning <br/>Date: 2011-11-02 to 2011-11-16", 
+          "label": "  Delive...", 
+          "dt": "Tue, 17 Jan 2015 06:30:00",
+          "rowId":5,
+          "customClass": "ganttGreen"
+        }
       }
     ]
   },
